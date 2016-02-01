ed9477a1ea7c9ac9810e40bb011aee08867f93e1
Verayuth <gonee171137@hotmail.com>
2016-02-01 13:17:38 +0700

    edit register
    แก้ไข หน้าสมัคร

--- PJ-Snablack/PJ-Snablack/App_Start/IdentityConfig.cs
+++ PJ-Snablack/PJ-Snablack/App_Start/IdentityConfig.cs
@@ -54,10 +54,10 @@ namespace PJ_Snablack
             manager.PasswordValidator = new PasswordValidator
             {
                 RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
-                RequireDigit = true,
-                RequireLowercase = true,
-                RequireUppercase = true,
+                RequireNonLetterOrDigit = false,
+                RequireDigit = false,
+                RequireLowercase = false,
+                RequireUppercase = false,
             };
 
             // Configure user lockout defaults
